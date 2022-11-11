ee86713e24d70206c62482c2b2cb69ae75b1d418
karimch21 <anonKM1999@mail.ru>
2022-11-11 20:45:42 +0300

    fear: add script stop promo video and show popap

--- songbird/js/index.js
+++ songbird/js/index.js
@@ -1,2 +1,21 @@
 import birdsData from '../assets/bd/birds.js'
+let welcomeBlock = document.querySelector('.welcome-block');
+let welcomeBlockInner = document.querySelector('.welcome-block__inner');
+let promoVideo = document.querySelector('.promo__video');
+let promo = document.querySelector('.promo');
 
+function addActiveClass(welcomeBlock, welcomeBlockInner) {
+    welcomeBlock.classList.add('welcome-block_active')
+    welcomeBlockInner.classList.add('welcome-block__inner_active')
+    promo.classList.add('promo_active')
+}
+
+
+promoVideo.addEventListener('timeupdate', () => {
+    let currentTime = promoVideo.currentTime;
+    let totalTime = promoVideo.duration;
+    if (currentTime === totalTime) {
+        promoVideo.pause()
+        addActiveClass(welcomeBlock, welcomeBlockInner)
+    }
+})
